bd781fd45a15f4f3944ef59da95e1936023ff73b
madiphilibert <philibertmadi@gmail.com>
2025-09-26 09:57:03 -0700

    Script to create scatter plot

--- 01_scripts/LDP_PR_palmerspenguins_cleaned_2025_09_10.R
+++ 01_scripts/LDP_PR_palmerspenguins_cleaned_2025_09_10.R
@@ -1,3 +1,13 @@
 ##load in raw data csv##
-raw_penguin_data<-read.csv("C:/Users/madip/OneDrive/Desktop/LDP_PR/LDP_Reproducibility_Productivity/00_raw_data/palmer_penguins_raw.csv")
+raw_penguin_data<-read.csv("C:/Users/madip/OneDrive/Desktop/LDP_PR/LDP_Reproducibility_Productivity/00_raw_data/raw_penguin_data.csv")
 
+##plot flipper and bill lengths##
+ggplot(data = raw_penguin_data, aes(x = bill_length_mm, y = flipper_length_mm, color = species)) +
+  geom_point(size = 3, alpha = 0.8) +
+  labs(
+    title = "Bill Length vs Flipper Length in Palmer Penguins",
+    x = "BIll Length (mm)",
+    y = "Flipper Length (mm)",
+    color = "Species"
+  ) +
+  theme_minimal()
